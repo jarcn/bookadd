adb7c8987845f710f5b810ea27614ac26bdedec2
chenjia <chenjia@qiyee-tech.com>
2023-03-27 15:02:35 +0800

    init

---
+++ add.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/jarcn/bookadd/add"
+	"github.com/jarcn/bookadd/internal/config"
+	"github.com/jarcn/bookadd/internal/server"
+	"github.com/jarcn/bookadd/internal/svc"
+
+	"github.com/zeromicro/go-zero/core/conf"
+	"github.com/zeromicro/go-zero/core/service"
+	"github.com/zeromicro/go-zero/zrpc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+var configFile = flag.String("f", "etc/add.yaml", "the config file")
+
+func main() {
+	flag.Parse()
+
+	var c config.Config
+	conf.MustLoad(*configFile, &c)
+	ctx := svc.NewServiceContext(c)
+
+	s := zrpc.MustNewServer(c.RpcServerConf, func(grpcServer *grpc.Server) {
+		add.RegisterAdderServer(grpcServer, server.NewAdderServer(ctx))
+
+		if c.Mode == service.DevMode || c.Mode == service.TestMode {
+			reflection.Register(grpcServer)
+		}
+	})
+	defer s.Stop()
+
+	fmt.Printf("Starting rpc server at %s...\n", c.ListenOn)
+	s.Start()
+}
---
+++ internal/logic/addlogic.go
@@ -0,0 +1,30 @@
+package logic
+
+import (
+	"context"
+
+	"github.com/jarcn/bookadd/add"
+	"github.com/jarcn/bookadd/internal/svc"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type AddLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewAddLogic(ctx context.Context, svcCtx *svc.ServiceContext) *AddLogic {
+	return &AddLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+func (l *AddLogic) Add(in *add.AddReq) (*add.AddResp, error) {
+	// todo: add your logic here and delete this line
+
+	return &add.AddResp{}, nil
+}
